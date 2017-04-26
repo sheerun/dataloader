5518f251431603c07278b41d6892595da3dbcd42
Adam Stankiewicz <sheerun@sher.pl>
2017-04-26 09:06:56 +0200

    Cleanup

--- mvp.rb
+++ mvp.rb
@@ -5,8 +5,6 @@ require 'byebug'
 require 'thread'
 require 'thwait'
 
-$mutex = Mutex.new
-
 class Promise
   def wait
     pending = Thread.current[:pending_batches]
@@ -41,22 +39,18 @@ class BatchPromise < Promise
     @dispatched
   end
 
-  def after_dispatch
-    @after_dispatch
-  end
+  attr_reader :after_dispatch
 
   def queue(key)
     if @dispatched
-      raise StandardError.new("Cannot queue elements after batch is dispatched. Queued key: #{key}")
+      raise StandardError, "Cannot queue elements after batch is dispatched. Queued key: #{key}"
     end
 
     @queue.push(key)
 
     @dispatch.then do |values|
       unless values.key?(key)
-        raise StandardError.new(
-          "Promise didn't resolve a key: #{key}\nResolved keys: #{values.keys.join(' ')}"
-        )
+        raise StandardError, "Promise didn't resolve a key: #{key}\nResolved keys: #{values.keys.join(' ')}"
       end
 
       values[key]
@@ -79,25 +73,19 @@ class BatchPromise < Promise
 
   def handle_result(keys, values)
     unless values.is_a?(Array) || values.is_a?(Hash)
-      raise TypeError.new(
-        'DataLoader must be constructed with a block which accepts ' +
-        'Array<key> and returns Array<value> or Hash<key, value>. ' +
+      raise TypeError, 'DataLoader must be constructed with a block which accepts ' \
+        'Array<key> and returns Array<value> or Hash<key, value>. ' \
         "Function returned instead: #{values}."
-      );
     end
 
     if keys.size != values.size
-      raise TypeError.new(
-        'DataLoader must be instantiated with function that returns Array or Hash ' +
-        'of the same size as provided to it Array of keys' +
-        "\n\nProvided keys:\n#{keys}" +
+      raise TypeError, 'DataLoader must be instantiated with function that returns Array or Hash ' \
+        'of the same size as provided to it Array of keys' \
+        "\n\nProvided keys:\n#{keys}" \
         "\n\nReturned values:\n#{values}"
-      );
     end
 
-    if values.is_a?(Array)
-      values = Hash[keys.zip(values)]
-    end
+    values = Hash[keys.zip(values)] if values.is_a?(Array)
 
     values
   end
@@ -106,10 +94,8 @@ end
 class DataLoader
   def initialize(options = {}, &batch_load)
     unless block_given?
-      raise TypeError.new(
-        'DataLoader must be constructed with a block which accepts ' +
+      raise TypeError, 'DataLoader must be constructed with a block which accepts ' \
         'Array<key> and returns Array<value> or Hash<key, value>'
-      )
     end
 
     @options = options
@@ -131,37 +117,36 @@ class DataLoader
 
   def load(key)
     if key.nil?
-      raise TypeError.new(
-        "The loader.load() must be called with a key, but got: #{key}"
-      )
+      raise TypeError, "The loader.load() must be called with a key, but got: #{key}"
     end
 
-    cache_key_fn = @options.fetch(:key, lambda { |key| key })
+    cache_key_fn = @options.fetch(:key, ->(key) { key })
 
-    if cache_key_fn.respond_to?(:call)
-      cache_key = cache_key_fn.call(key)
-    else
-      cache_key = key[cache_key_fn]
-    end
+    cache_key = if cache_key_fn.respond_to?(:call)
+                  cache_key_fn.call(key)
+                else
+                  key[cache_key_fn]
+                end
 
-    @promises.compute_if_absent(cache_key) {
+    @promises.compute_if_absent(cache_key) do
       batch = batch_promise
       batch.queue(key)
-    }
+    end
   end
 
   def batch_promise
     if @batch_promise.nil? || @batch_promise.dispatched?
       new_promise = create_batch_promise
-      Thread.start do
-        sleep 0.05
-        new_promise.dispatch
-      end if @batch_promise
+      if @batch_promise
+        Thread.start do
+          sleep 0.05
+          new_promise.dispatch
+        end
+      end
       @batch_promise = new_promise
-      @batch_promise
-    else
-      @batch_promise
     end
+
+    @batch_promise
   end
 
   def create_batch_promise
@@ -169,10 +154,8 @@ class DataLoader
   end
 
   def load_many(keys)
-    unless keys.kind_of?(Array)
-      raise TypeError.new(
-        "The loader.load_many() must be called with a Array<key>, but got: #{key}"
-      )
+    unless keys.is_a?(Array)
+      raise TypeError, "The loader.load_many() must be called with a Array<key>, but got: #{key}"
     end
 
     Promise.all(keys.map(&method(:load)))
@@ -181,35 +164,32 @@ class DataLoader
   def dispatch
     @batch_promise.dispatch if @batch_promise && !@batch_promise.dispatched?
   end
-
 end
 
-prefix = "Name"
-
 loader = DataLoader.new do |ids|
-  puts "Loading records: #{ids.join(" ")}"
+  puts "Loading records: #{ids.join(' ')}"
   Hash[ids.zip(ids.map { |id| { id: id, name: "Something #{id}" } })]
 end
 
-loader2 = DataLoader.new(name: "names") do |ids|
-  puts "Loading names: #{ids.join(" ")}"
+loader2 = DataLoader.new(name: 'names') do |ids|
+  puts "Loading names: #{ids.join(' ')}"
   loader.load_many(ids).then do |records|
     Hash[ids.zip(records.map { |r| r[:name] })]
   end
 end
 
-loader3 = DataLoader.new(name: "awesome") do |ids|
-  puts "Loading awesome names: #{ids.join(" ")}"
+loader3 = DataLoader.new(name: 'awesome') do |ids|
+  puts "Loading awesome names: #{ids.join(' ')}"
   loader2.load_many(ids).then do |names|
     Hash[ids.zip(names.map { |name| "Awesome #{name}" })]
   end
 end
 
 one = loader.load(0)
-two = loader.load_many([1,2])
-three = loader.load_many([2,3])
-four = loader2.load_many([2,3,5])
-five = loader3.load_many([2,3,5,7])
+two = loader.load_many([1, 2])
+three = loader.load_many([2, 3])
+four = loader2.load_many([2, 3, 5])
+five = loader3.load_many([2, 3, 5, 7])
 
 DataLoader.dispatch
 
