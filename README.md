b5660d7822cc73a76496d13a083be0a3da668bd9
Adam Stankiewicz <sheerun@sher.pl>
2017-04-29 18:15:09 +0200

    Add some more documentation

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# ![](http://i.imgur.com/i0afc40.png) Dataloader 
+# ![](http://i.imgur.com/i0afc40.png) Dataloader
 
 [![Build Status](https://travis-ci.org/sheerun/dataloader.svg?branch=master)](https://travis-ci.org/sheerun/dataloader) [![codecov](https://codecov.io/gh/sheerun/dataloader/branch/master/graph/badge.svg)](https://codecov.io/gh/sheerun/dataloader)
 
@@ -28,7 +28,50 @@ user0 = promise_one.sync
 user1, user2 = promise_two.sync
 ```
 
+## API
 
+### `Dataloader`
+
+`Dataloader` is a class for fetching data given unique keys such as the id column (or any other key).
+
+Each `Dataloader` instance contains a unique memoized cache. Because of it, it is recommended to use one `Datalaoder` instane **per web request**. You can use more long-lived instances, but then you need to take care of manually cleaning the cache.
+
+You shound't share the same dataloader instance across different threads. This behavior is currently undefined.
+
+### `Dataloader.new(options = {}, &batch_load)`
+
+Create a new `Dataloader` given a batch loading function and options.
+
+* `batch_load`: A block which accepts an Array of keys, and returns  Array of values or Hash that maps from keys to values (or a [Promise](https://github.com/lgierth/promise.rb) that returns such value).
+* `options`: An optional hash of options:
+  * `:key` A function to produce a cache key for a given load key. Defaults to proc { |key| key }. Useful to provide when objects are keys and two similarly shaped objects should be considered equivalent.
+
+
+### `#load(key)`
+
+Loads a key, returning a [Promise](https://github.com/lgierth/promise.rb) for the value represented by that key.
+
+* key: An key value to load (can be any object)
+
+You can resolve this promise when you actually need the value with `promise.sync`.
+
+All calls to `#load` are batched until the first `#sync` is encountered. Then is starts batching again, et caetera.
+
+### `#load_many(keys)`
+
+Loads multiple keys, promising an array of values:
+
+```ruby
+promise = loader.load_many(['a', 'b'])
+object_a, object_b = promise.sync
+```
+
+This is equivalent to the more verbose:
+
+```ruby
+promise = Promise.all([loader.load('a'), loader.load('b')])
+object_a, object_b = promise.sync
+```
 
 ## License
 
--- lib/dataloader.rb
+++ lib/dataloader.rb
@@ -34,7 +34,7 @@ class Dataloader
     def dispatch
       @dispatched = true
 
-      result = @dataloader.load_batch.call(@queue)
+      result = @dataloader.batch_load.call(@queue)
 
       if result.is_a?(Promise)
         result.then do |values|
@@ -89,16 +89,21 @@ class Dataloader
 
   private_constant :Batch
 
-  attr_reader :load_batch
+  attr_reader :batch_load
 
-  def initialize(options = {}, &load_batch)
+  # Creates new dataloader
+  #
+  # @option options [Proc] :key A function to produce a cache key for a given load key. Defaults to proc { |key| key }. Useful to provide when objects are keys and two similarly shaped objects should be considered equivalent.
+  # @yieldparam [Array] array is batched ids to load
+  # @yieldreturn [Promise] a promise of loaded value with batch_load block
+  def initialize(options = {}, &batch_load)
     unless block_given?
       raise TypeError, "Dataloader must be constructed with a block which accepts " \
         "Array<Object> and returns Array<Object> or Hash<Object, Object>"
     end
 
-    @options = options
-    @load_batch = load_batch
+    @key = options.fetch(:key, lambda { |key| key })
+    @batch_load = batch_load
     @cache = Concurrent::Map.new
 
     Thread.current[:pending_batches] ||= []
@@ -112,6 +117,24 @@ class Dataloader
     end
   end
 
+  # Loads a key, returning a [Promise](https://github.com/lgierth/promise.rb) for the value represented by that key.
+  # 
+  # You can resolve this promise when you actually need the value with `promise.sync`.
+  # 
+  # All calls to `#load` are batched until the first `#sync` is encountered. Then is starts batching again, et caetera.
+  #
+  # @param key [Object] key to load using `batch_load` proc
+  # @return [Promise<Object>] A Promise of computed value
+  # @example Load promises of two users and resolve them:
+  #   user_loader = Dataloader.new do |ids|
+  #     User.find(*ids)
+  #   end
+  #
+  #   user1_promise = user_loader.load(1)
+  #   user2_promise = user_loader.load(2)
+  #
+  #   user1 = user1_promise.sync
+  #   user2 = user2_promise.sync
   def load(key)
     if key.nil?
       raise TypeError, "The loader.load() must be called with a key, but got: #{key}"
@@ -122,6 +145,32 @@ class Dataloader
     end
   end
 
+  # 
+  #
+  # Loads multiple keys, promising an array of values:
+  # 
+  # ```ruby
+  # promise = loader.load_many(['a', 'b'])
+  # object_a, object_b = promise.sync
+  # ```
+  # 
+  # This is equivalent to the more verbose:
+  # 
+  # ```ruby
+  # promise = Promise.all([loader.load('a'), loader.load('b')])
+  # object_a, object_b = promise.sync
+  # ```
+  #
+  # @param keys [Array<Object>] list of keys to load using `batch_load` proc
+  # @return [Promise<Object>] A Promise of computed values
+  # @example Load promises of two users and resolve them:
+  #   user_loader = Dataloader.new do |ids|
+  #     User.find(*ids)
+  #   end
+  #
+  #   users_promise = user_loader.load_many([1, 2])
+  #
+  #   user1, user2 = users_promise.sync
   def load_many(keys)
     unless keys.is_a?(Array)
       raise TypeError, "The loader.load_many() must be called with a Array<Object>, but got: #{key}"
@@ -133,9 +182,7 @@ class Dataloader
   protected
 
   def compute_if_absent(key)
-    cache_key = @options.key?(:key) ? @options.key.call(key) : key
-
-    @cache.compute_if_absent(cache_key) do
+    @cache.compute_if_absent(@key.call(key)) do
       yield
     end
   end
